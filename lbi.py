8215cd3ba45ba5981affbf909297b495e3985a3f
martincss <martincarusso@gmail.com>
2017-08-29 06:05:26 -0300

    en principio terminado poiseuille, se recuperan bien los resultados!! es estable con omega = 0.9

--- lbi.py
+++ lbi.py
@@ -6,22 +6,26 @@ Created on Fri Aug 25 04:11:42 2017
 """
 
 import numpy as np
-from numpy import linspace, arange, zeros, ones, pi
+from numpy import linspace, zeros, ones, pi, sum
 import matplotlib.pyplot as plt
 
 #%% Definimos las dimesiones espaciales y temporales
 
-N_x = 10; X = np.arange(N_x)
-N_y = 10; Y = np.arange(N_y)
-t_max = 5
+N_x = 16; X = linspace(0,1,N_x)
+N_y = 32; Y = linspace(0,2,N_y)
+t_max = 300
 q = 9
-tau = 3.5
-omega = 1/tau
-delta_x = 1
-delta_t = 1
+omega = 0.9
+tau = 1/omega
+nu = 1
+delta_x = 2/N_y
+delta_t = (2*tau-1)/6 *(delta_x)**2/nu
 c = delta_x/delta_t 
 
-params_slg = [5/12, 1/3, 1/12] # los parámetros sigma, lambda y gamma
+sigma = 5/12
+lambdaa = 1/3
+gamma = 1/12
+params_slg = [-4*sigma, lambdaa, lambdaa, lambdaa, lambdaa, gamma, gamma, gamma, gamma ] # los parámetros sigma, lambda y gamma
 w = [4/9, 1/9, 1/9, 1/9, 1/9, 1/36, 1/36, 1/36, 1/36] # los pesos
 
 g_old = zeros((N_x, N_y, q)) 
@@ -37,21 +41,27 @@ u_x = zeros((N_x, N_y))
 u_y = zeros((N_x, N_y))
 #u_y[1,:] = ones(N_y)
 
-e = zeros((9,2))
-for i in range(1,5):
-    e[i,0] = np.cos((i-1)*pi/2)
-    e[i,1] = np.sin((i-1)*pi/2)
-
-for i in range(5,9):
-    e[i,0] = np.sqrt(2)*(np.cos((i-5)*pi/2 + pi/4))
-    e[i,1] = np.sqrt(2)*(np.sin((i-5)*pi/2 + pi/4))
+def generar_d2q9():
+    e = zeros((9,2))
+    for i in range(1,5):
+        e[i,0] = np.cos((i-1)*pi/2)
+        e[i,1] = np.sin((i-1)*pi/2)
+    
+    for i in range(5,9):
+        e[i,0] = np.sqrt(2)*(np.cos((i-5)*pi/2 + pi/4))
+        e[i,1] = np.sqrt(2)*(np.sin((i-5)*pi/2 + pi/4))
+        
+    return e
     
 
 #%% Generación de variables macroscópicas (moment update)
 
-def generar_p(g, sigma, u_x, u_y, w, e, c):
+def generar_p(g, sigma, u_x, u_y, w, c, p_inlet, p_outlet):
+    N_y = g.shape[1]
     # en la sum venia el axis = 2
-    pres = c**2/(4*sigma)*( np.sum(g[:,:,1:]) + s(0, u_x, u_y, w, e, c) )
+    pres = c**2/(4*sigma)*( sum(g[:,:,1:], axis = 2) + s(0, u_x, u_y, w, c) )
+    pres[:,0] = p_inlet
+    pres[:,N_y-1] = p_outlet
     return pres
 
 def generar_vel(g, c):
@@ -61,24 +71,18 @@ def generar_vel(g, c):
 
 #%% Distribución de equilibrio a partir de rho y u
 
-def s(i, u_x, u_y, w, e, c):
+def s(i, u_x, u_y, w, c):
+    e = generar_d2q9()
     si = w[i]*( 3*(e[i,0]*u_x + e[i,1]*u_y)/c + 4.5*((e[i,0]*u_x + e[i,1]*u_y)**2)/(c**2) - 1.5*(u_x**2 + u_y**2)/(c**2) )
     return si
 
-def generar_eq(p, u_x, u_y, e, slg, c, g_eq):
+def generar_eq(p, u_x, u_y, slg, c, g_eq):
     """
     A partir de rho y u_x, u_y genera valores la distribución de equilibrio 
     g_eq según la página 93
     """
-    g_eq[:,:,0] = -4*slg[0]*p/(c**2) + s(0, u_x, u_y, w, e, c)
-    g_eq[:,:,1] = slg[1]*p/(c**2) + s(1, u_x, u_y, w, e, c)
-    g_eq[:,:,2] = slg[1]*p/(c**2) + s(2, u_x, u_y, w, e, c)
-    g_eq[:,:,3] = slg[1]*p/(c**2) + s(3, u_x, u_y, w, e, c)
-    g_eq[:,:,4] = slg[1]*p/(c**2) + s(4, u_x, u_y, w, e, c)
-    g_eq[:,:,5] = slg[2]*p/(c**2) + s(5, u_x, u_y, w, e, c)
-    g_eq[:,:,6] = slg[2]*p/(c**2) + s(6, u_x, u_y, w, e, c)
-    g_eq[:,:,7] = slg[2]*p/(c**2) + s(7, u_x, u_y, w, e, c)
-    g_eq[:,:,8] = slg[2]*p/(c**2) + s(8, u_x, u_y, w, e, c)
+    for i in range(9):
+        g_eq[:,:,i] = slg[i]*p/(c**2) + s(i, u_x, u_y, w, c)
     
 
 #%% Relajación mediante colisiones
@@ -153,114 +157,44 @@ def bounce_inf(g_old, g_new):
         g_new[piso,j,7] = g_old[piso-1,j+1,7]
         g_new[piso,j,8] = g_old[piso-1,j-1,8]
 
-def pressure_left(g_old, g_new, slg, p_inlet, u_x, u_y, w, e, c, omega):
+# usar la old en estas? (en el termino de 1-w)
+def pressure_left(g_old, g_new, slg, p_inlet, u_x, u_y, w, c, omega):
+
+    N_x = g_old.shape[0]
+   
+    g_bar = zeros((N_x,9))
+    for k in range(N_x):
+        for i in range(9):
+            g_bar[k,i] = slg[i]*p_inlet/(c**2) + s(i, u_x[k,1], u_y[k,1], w, c)
+        g_new[k,0,:] = g_bar[k,:] + (1-omega)*(g_old[k,1,:] - g_eq[k,1,:])
+
+
+def pressure_right(g_old, g_new, slg, p_outlet, u_x, u_y, w, c, omega):
     
     N_x = g_old.shape[0]
     N_y = g_old.shape[1]
     
     g_bar = zeros((N_x,9))
     for k in range(N_x):
-         g_bar[k,0] = 4*slg[0]*p_inlet/(c**2) + s(0, u_x, u_y, w, e, c)[k,1]
-         g_bar[k,1] = slg[1]*p_inlet/(c**2) + s(1, u_x, u_y, w, e, c)[k,1]
-         g_bar[k,2] = slg[1]*p_inlet/(c**2) + s(2, u_x, u_y, w, e, c)[k,1]
-         g_bar[k,3] = slg[1]*p_inlet/(c**2) + s(3, u_x, u_y, w, e, c)[k,1]
-         g_bar[k,4] = slg[1]*p_inlet/(c**2) + s(4, u_x, u_y, w, e, c)[k,1]
-         g_bar[k,5] = slg[2]*p_inlet/(c**2) + s(5, u_x, u_y, w, e, c)[k,1]
-         g_bar[k,6] = slg[2]*p_inlet/(c**2) + s(6, u_x, u_y, w, e, c)[k,1]
-         g_bar[k,7] = slg[2]*p_inlet/(c**2) + s(7, u_x, u_y, w, e, c)[k,1]
-         g_bar[k,8] = slg[2]*p_inlet/(c**2) + s(8, u_x, u_y, w, e, c)[k,1]
+        for i in range(9):
+            g_bar[k,i] = slg[i]*p_outlet/(c**2) + s(i, u_x[k,N_y-2], u_y[k,N_y-2], w, c)      
+        g_new[k,N_y-1,:] = g_bar[k,:] + (1-omega)*(g_old[k,N_y-2,:] - g_eq[k,N_y-2,:])
          
-         g_new[k,0,:] = g_bar[k,:] + (1-omega)*(g_new[k,1,:] - g_eq[k,1,:])
-
-
-def pressure_right(g_old, g_new, slg, p_outlet, u_x, u_y, w, e, c, omega):
-    
-    N_x = g_old.shape[0]
-    N_y = g_old.shape[1]
-    
-    g_bar = zeros((N_x,9))
-    for k in range(N_x):
-         g_bar[k,0] = 4*slg[0]*p_outlet/(c**2) + s(0, u_x, u_y, w, e, c)[k,N_y-2]
-         g_bar[k,1] = slg[1]*p_outlet/(c**2) + s(1, u_x, u_y, w, e, c)[k,N_y-2]
-         g_bar[k,2] = slg[1]*p_outlet/(c**2) + s(2, u_x, u_y, w, e, c)[k,N_y-2]
-         g_bar[k,3] = slg[1]*p_outlet/(c**2) + s(3, u_x, u_y, w, e, c)[k,N_y-2]
-         g_bar[k,4] = slg[1]*p_outlet/(c**2) + s(4, u_x, u_y, w, e, c)[k,N_y-2]
-         g_bar[k,5] = slg[2]*p_outlet/(c**2) + s(5, u_x, u_y, w, e, c)[k,N_y-2]
-         g_bar[k,6] = slg[2]*p_outlet/(c**2) + s(6, u_x, u_y, w, e, c)[k,N_y-2]
-         g_bar[k,7] = slg[2]*p_outlet/(c**2) + s(7, u_x, u_y, w, e, c)[k,N_y-2]
-         g_bar[k,8] = slg[2]*p_outlet/(c**2) + s(8, u_x, u_y, w, e, c)[k,N_y-2]
-         
-         g_new[k,N_y-1,:] = g_bar[k,:] + (1-omega)*(g_new[k,N_y-2,:] - g_eq[k,N_y-2,:])
-         
-    
-def pressure_left2(g_old, g_new, e, w, u_x, u_y, p_inlet):
-    """
-    Realiza la advección para los puntos del borde izquierdo del recinto,
-    con la condición de contorno de presión fija, según el método 'anti-bounce
-    back' (libro LB ss 5.3.5.2)
-    """
-    
-    N_x = g_old.shape[0]
-    N_y = g_old.shape[1]
-    
-    # Primero las direcciones que advectan desde la derecha hacia el borde izquierdo
-    for i in range(1,N_x-1):
-        g_new[i,0,0] = g_old[i,0,0]
-        g_new[i,0,2] = g_old[i+1,0,2]
-        g_new[i,0,3] = g_old[i,1,3]
-        g_new[i,0,4] = g_old[i-1,0,4]
-        g_new[i,0,6] = g_old[i+1,1,6]
-        g_new[i,0,7] = g_old[i-1,1,7]
-    
-    # Ahora los que "rebotan" contra el borde   
-    for i in range(1,N_x-1):
-        # La velocidad en la pared
-        u_wx = u_x[i,0] + 0.5*(u_x[i,0] - u_x[i,1])
-        u_wy = u_y[i,0] + 0.5*(u_y[i,0] - u_y[i,1])
-        g_new[i,0,1] = -g_old[i,0,1] + 6*p_inlet*w[1]*(1 + (e[1,0]*u_wx + e[1,1]*u_wy)**2/(2/9) - (u_wx**2 + u_wy**2)/(2/3) )
-        g_new[i,0,5] = -g_old[i,0,5] + 6*p_inlet*w[5]*(1 + (e[5,0]*u_wx + e[5,1]*u_wy)**2/(2/9) - (u_wx**2 + u_wy**2)/(2/3) )
-        g_new[i,0,1] = -g_old[i,0,8] + 6*p_inlet*w[8]*(1 + (e[8,0]*u_wx + e[8,1]*u_wy)**2/(2/9) - (u_wx**2 + u_wy**2)/(2/3) )
-
-
-def pressure_right2(g_old, g_new, e, w, u_x, u_y, p_outlet):
-    """
-    Realiza la advección para los puntos del borde derecho del recinto,
-    con la condición de contorno de presión fija, según el método 'anti-bounce
-    back' (libro LB ss 5.3.5.2)
-    """
-    
-    N_x = g_old.shape[0]
-    N_y = g_old.shape[1]
-    
-    # Primero las direcciones que advectan desde la izquerda hacia el borde derecho
-    for i in range(1,N_x-1):
-        g_new[i,N_y-1,0] = g_old[i,N_y-1,0]
-        g_new[i,N_y-1,1] = g_old[i,N_y-2,1]
-        g_new[i,N_y-1,2] = g_old[i+1,N_y-1,2]
-        g_new[i,N_y-1,4] = g_old[i-1,N_y-1,4]
-        g_new[i,N_y-1,5] = g_old[i+1,N_y-2,5]
-        g_new[i,N_y-1,8] = g_old[i-1,N_y-2,8]
-    
-    # Ahora los que "rebotan" contra el borde   
-    for i in range(1,N_x-1):
-        # La velocidad en la pared
-        u_wx = u_x[i,N_y-1] + 0.5*(u_x[i,N_y-1] - u_x[i,N_y-2])
-        u_wy = u_y[i,N_y-1] + 0.5*(u_y[i,N_y-1] - u_y[i,N_y-2])
-        g_new[i,N_y-1,3] = -g_old[i,N_y-1,3] + 6*p_outlet*w[3]*(1 + (e[3,0]*u_wx + e[3,1]*u_wy)**2/(2/9) - (u_wx**2 + u_wy**2)/(2/3) )
-        g_new[i,N_y-1,6] = -g_old[i,N_y-1,6] + 6*p_outlet*w[6]*(1 + (e[6,0]*u_wx + e[6,1]*u_wy)**2/(2/9) - (u_wx**2 + u_wy**2)/(2/3) )
-        g_new[i,N_y-1,7] = -g_old[i,N_y-1,7] + 6*p_outlet*w[7]*(1 + (e[7,0]*u_wx + e[7,1]*u_wy)**2/(2/9) - (u_wx**2 + u_wy**2)/(2/3) )
-
 
 #%% Evolución temporal
 
-def time_evol(g_eq, g_old, g_new, pres, u_x, u_y, params_slg, w, e, c, omega, p_inlet, p_outlet):
+def time_evol(g_eq, g_old, g_new, pres, u_x, u_y, params_slg, w, c, omega, sigma, p_inlet, p_outlet):
     """
     """
     # A partir de p y u, genero la g de equilibrio
-    generar_eq(pres, u_x, u_y, e, params_slg, c, g_eq)
+    generar_eq(pres, u_x, u_y, params_slg, c, g_eq)
     
     # Hago la relajación por colisiones
     coll(g_old, g_eq, tau)
+    
+    #acá vienen los contornos para presion/vel
+    pressure_left(g_old, g_new, params_slg, p_inlet, u_x, u_y, w, c, omega)
+    pressure_right(g_old, g_new, params_slg, p_outlet, u_x, u_y, w, c, omega)
     
     # Hago la advección en el interior
     advect(g_old, g_new)
@@ -268,33 +202,35 @@ def time_evol(g_eq, g_old, g_new, pres, u_x, u_y, params_slg, w, e, c, omega, p_
     # Hago la advección en los bordes con las condiciones de contorno
     bounce_sup(g_old, g_new)
     bounce_inf(g_old, g_new)
-    #acá vienen los contornos para presion/vel
-    pressure_left(g_old, g_new, params_slg, p_inlet, u_x, u_y, w, e, c, omega)
-    pressure_right(g_old, g_new, params_slg, p_outlet, u_x, u_y, w, e, c, omega)
+    
     
     # Actualizo la presión y velocidad macro
     u_xnew, u_ynew = generar_vel(g_new, c)
-    pres_new = generar_p(g_new, params_slg[0], u_xnew, u_ynew, w, e, c)
+    pres_new = generar_p(g_new, sigma, u_xnew, u_ynew, w, c, p_inlet, p_outlet)
     g_old = g_new
-    
     return pres_new, u_xnew, u_ynew
 #
     
 #%% Acá el main
 
-generar_eq(pres, u_x, u_y, e, params_slg, c, g_old)
-for i in range(12):
-    pres, u_x, u_y = time_evol(g_eq, g_old, g_new, pres, u_x, u_y, params_slg, w, e, c, omega, p_inlet, p_outlet)
+generar_eq(pres, u_x, u_y, params_slg, c, g_old)
+for i in range(t_max):
+    pres, u_x, u_y = time_evol(g_eq, g_old, g_new, pres, u_x, u_y, params_slg, w, c, omega, sigma, p_inlet, p_outlet)
 
             
 #%% Ploteo
 
 plt.figure()
 xx, yy = np.meshgrid(X,Y)
-plt.quiver(xx, yy, u_x, u_y)
-            
-            
-            
+plt.quiver(yy, xx, u_x, u_y)
+
+plt.figure()
+for i in range(N_y):
+    plt.plot(X, u_x[:,i])
+
+plt.figure()            
+for i in range(N_x):
+    plt.plot(Y, pres[i,:])
             
             
     
